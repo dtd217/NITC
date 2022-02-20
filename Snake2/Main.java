813b5619941fe16d7090606c74b30a85c25a2642
dtd217 <duydong217@gmail.com>
2022-02-20 21:33:45 +0700

    add title, icon

--- Snake2/Main.java
+++ Snake2/Main.java
@@ -2,9 +2,11 @@ package org.nitc.Snake2;
 
 import javafx.application.Application;
 import java.io.IOException;
+import java.io.InputStream;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +15,8 @@ public class Main extends Application {
 
 	@Override
     public void start(Stage stage) throws IOException {
+		stage.setTitle("Snake game");
+		stage.getIcons().add(new Image("https://cdn2.iconfinder.com/data/icons/animals-nature-2/50/1F40D-snake-512.png"));
         Scene scene = new Scene(loadFXML("layout"), 640, 480);
         stage.setScene(scene);
         stage.show();
